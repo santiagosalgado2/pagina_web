94aed68d30deee8b9288d2d6251f7661509c0dcc
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-09-13 19:10:10 -0300

    estilo inicio

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina principal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url("/css/iniciostyle.css");?>">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
