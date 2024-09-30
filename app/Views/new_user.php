cb119eb7b447041b1ea2f9763f8d747a340d0ab7
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-09-30 19:03:16 -0300

    backgrounds

--- app/Views/new_user.php
+++ app/Views/new_user.php
@@ -3,11 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("/css/estilog.css");?>">
     <title>Document</title>
 </head>
 <body>
     
-    <h1 style="color: #0e68aa;">Ingrese los datos del usuario que desea crear</h1>
+    <div class="circulo"></div>
+    <div class="circulo"></div>
+    <div class="circulo"></div>
+    <div class="circulo"></div>
+
+    <h1 style="color: black;">Ingrese los datos del usuario que desea crear</h1>
 
     <form method="post" action="<?php echo base_url("/create_user") ;?>">
 
