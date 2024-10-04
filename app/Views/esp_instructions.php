0edf1e5809e57e53d0e210d016cb81daf67b3a31
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-10-04 14:20:34 -0300

    css

--- app/Views/esp_instructions.php
+++ app/Views/esp_instructions.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("/css/estilog.css");?>"> 
     <title>Document</title>
 </head>
 <body>
+
+<div class="circulo"></div>
+<div class="circulo"></div>
+<div class="circulo"></div>
+<div class="circulo"></div>
+
 <h2>Sigue los siguientes pasos para vincular un nuevo Esp32</h2>
 
 <ul>
--- app/Views/new_esp.php
+++ app/Views/new_esp.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("/css/estilog.css");?>"> 
     <title>Document</title>
 </head>
 <body>
+
+    <div class="circulo"></div>
+    <div class="circulo"></div>
+    <div class="circulo"></div>
+    <div class="circulo"></div>
+    
     <form method="post" action="<?php echo base_url("/new_esp/insert"); ?>">
 
         <input type="text" name="code">Ingrese el código ubicado en la caja de la Esp32
