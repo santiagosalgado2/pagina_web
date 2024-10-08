b72dcc0474ee3642c1333b60b23250cf30f7bab6
santiagosalgado <santiagosalgado@alumnos.itr3.edu.ar>
2024-10-08 10:46:18 -0300

    nada

--- app/Controllers/Esp32C.php
+++ app/Controllers/Esp32C.php
@@ -22,37 +22,6 @@ class Esp32C extends BaseController{
 
     }
 
-    public function receiveConn(){
-        $param1 = $this->request->getPost('param1');
-        $param2 = $this->request->getPost('param2');
-        
-        // Log de depuración
-        log_message('info', "param1: $param1");
-        log_message('info', "param2: $param2");
-        
-        if ($param1 && $param2) {
-            echo "Datos recibidos: param1=$param1, param2=$param2";
-        } else {
-            echo "Datos no recibidos";
-        }
-        
-    
-    }
-
-    public function sendIR() {
-        $esp32IP = '10.81.11.233'; // Cambia esto por la IP de tu ESP32
-        $url = "http://$esp32IP/sendIR";
-
-        $response = file_get_contents($url); // Envía la solicitud GET
-
-        if ($response === FALSE) {
-        // Manejo del error
-            echo "Error al enviar la solicitud.";
-        } else {
-            echo $response; // Muestra la respuesta de la ESP32
-        }
-    }
-
     public function newEspview(){
         return view('new_esp');
     }
@@ -132,6 +101,39 @@ class Esp32C extends BaseController{
 
         
 
+    }
+
+
+    #FUNCIONES DE PRUEBA
+    public function receiveConn(){
+        $param1 = $this->request->getPost('param1');
+        $param2 = $this->request->getPost('param2');
+        
+        // Log de depuración
+        log_message('info', "param1: $param1");
+        log_message('info', "param2: $param2");
+        
+        if ($param1 && $param2) {
+            echo "Datos recibidos: param1=$param1, param2=$param2";
+        } else {
+            echo "Datos no recibidos";
+        }
+        
+    
+    }
+
+    public function sendIR() {
+        $esp32IP = '10.81.11.233'; // Cambia esto por la IP de tu ESP32
+        $url = "http://$esp32IP/sendIR";
+
+        $response = file_get_contents($url); // Envía la solicitud GET
+
+        if ($response === FALSE) {
+        // Manejo del error
+            echo "Error al enviar la solicitud.";
+        } else {
+            echo $response; // Muestra la respuesta de la ESP32
+        }
     }
 
 }
