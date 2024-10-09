abac3431e56326d0245bd574c563eb3e9d070807
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-10-08 21:49:09 -0300

    no c

--- app/Views/userinfo.php
+++ app/Views/userinfo.php
@@ -67,8 +67,8 @@
     <div class="container">
         <h1>Informacion de tu usuario</h1>
             <ul>
-                <li><b>Nombre de usuario: </b><?php echo $data[0]["nombre_usuario"]; ?>   <a href="<?php echo base_url("/change_user"); ?>"><button>Cambiar</button></a></li>
-                <li><b>Contraseña: </b>********  <a href=<?php echo base_url("/generate/recuperar_contrasena"); ?>><button>Reestablecer</button></a></li>
+                <li><b>Nombre de usuario: </b><?php echo $data[0]["nombre_usuario"]; ?>   <a href="<?php echo base_url("/change_user"); ?>"><button class="button2">Cambiar</button></a></li>
+                <li><b>Contraseña: </b>********  <a href=<?php echo base_url("/generate/recuperar_contrasena"); ?>><button class="button2">Reestablecer</button></a></li>
                 <li><b>Dirección de e-mail: </b><?php echo $data[0]["email"]; ?>   <a href="<?php echo base_url("/change_email"); ?>"></a></li>
                 <li><b>Fecha de creación: </b><?php echo $data[0]["fecha_creacion"]; ?></li>
                 <?php if(isset($admin)):?>
@@ -77,7 +77,7 @@
                 <?php else:?>
                 <li><b>Tipo de usuario: </b>Administrador</li>
                 <?php endif;?>
-                <a href="<?php echo base_url("/");?>"><button>Volver</button></a>
+                <a href="<?php echo base_url("/");?>"><button class="button2">Volver</button></a>
             </ul>
     </div>        
     </center>  
@@ -105,8 +105,85 @@
             box-shadow: 0 12px 30px rgba(0, 0, 0, 0.3); /* Sombra más pronunciada */
             width: 600px; /* Ajuste del ancho */
             border: 2px solid #ff9999; /* Borde suave */
+            font-size: 18px;
         }
-        
+        button:hover {
+            opacity: 1;
+        }
+
+        button:active {
+            top: 4px;
+            box-shadow: #c0392b 0px 3px 2px,#000 0px 3px 5px;
+        }
+        .button2 {
+            display: inline-block;
+            transition: all 0.2s ease-in;
+            position: relative;
+            overflow: hidden;
+            z-index: 1;
+            color: #090909;
+            padding: 0.2em 0,5em;
+            cursor: pointer;
+            font-size: 14px;
+            border-radius: 0.5em;
+            background: #e8e8e8;
+            border: 1px solid #e8e8e8;
+            box-shadow: 1,5px 1,5px 4px #c5c5c5, 0px 0px 3px #ffffff;
+  
+        }
+
+        .button2:active {
+            color: #666;
+            box-shadow: inset 4px 4px 12px #c5c5c5, inset -4px -4px 12px #ffffff;
+        }
+
+        .button2:before {
+            content: "";
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%) scaleY(1) scaleX(1.25);
+            top: 100%;
+            width: 140%;
+            height: 180%;
+            background-color: rgba(0, 0, 0, 0.05);
+            border-radius: 50%;
+            display: block;
+            transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+            z-index: -1;
+        }
+
+        .button2:after {
+            content: "";
+            position: absolute;
+            left: 55%;
+            transform: translateX(-50%) scaleY(1) scaleX(1.45);
+            top: 180%;
+            width: 160%;
+            height: 190%;
+            background-color: #fa8560;
+            border-radius: 50%;
+            display: block;
+            transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+            z-index: -1;
+        }
+
+        .button2:hover {
+            color: #ffffff;
+            border: 1px solid #fa8560;
+        }
+
+        .button2:hover:before {
+            top: -35%;
+            background-color: #fa8560;
+            transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
+        }
+
+        .button2:hover:after {
+            top: -45%;
+            background-color: #fa8560;
+            transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
+        }
+
     </style>  
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>   
 </body>
