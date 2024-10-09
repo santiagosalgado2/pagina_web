02d37ceaae4b7b519b5f7583efc53642a8d896c0
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-10-09 16:45:46 -0300

    AIRCONTROL VIEW(NO TIENE RUTA)

---
+++ app/Views/aire.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("/css/airstyle.css");?>"> 
+    <title>Control Remoto</title>
+</head>
+<body>
+<div class="control-remoto">
+        <div class="pantalla">AIRE TV</div>
+        
+        <div class="seccion">
+            <div class="temp-botones">
+                <div class="boton">TEMP ▲</div>
+                <div class="boton">TEMP ▼</div>
+            </div>
+        </div>
+
+        <div class="seccion">
+            <div class="power-boton">
+                <div class="boton rojo">ON/OFF</div>
+            </div>
+        </div>
+
+        <div class="seccion">
+            <div class="boton-container">
+                <div class="boton-fan">FAN SPEED</div>
+                <div class="boton">MODE</div>
+            </div>
+        </div>
+    
+        <div class="seccion">
+            <div class="boton-container">
+            
+                <div class="boton">SWING</div>
+                <div class="boton">SLEEP</div>
+            </div>
+        </div>
+        
+        <div class="seccion">
+            <div class="boton-container">
+                <div class="boton-on">TIMER ON</div>
+                <div class="boton-timer">TIMER OFF</div>
+            </div>
+        </div>
+        
+        <div class="seccion">
+            <div class="boton-container">
+                <div class="boton-led">LED DISPLAY</div>
+                <div class="boton">TURBO</div>
+            </div>
+        </div>
+        <div class="seccion">
+                <div class="boton-direc">DIREC</div>
+            </div>
+    </div>
+</body>
+</html>
+
