60de924f1d83c274cebf3bc1c33b567db3970d97
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-10-16 16:02:27 -0300

    cree los estilos(.CSS) de algunos archivos

--- app/Views/userinfo.php
+++ app/Views/userinfo.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url("/css/estilog.css");?>">
+    <link rel="stylesheet" href="<?php echo base_url("/css/userinfostyle.css");?>">
     <title>Document</title>
 </head>
 <body>
@@ -81,118 +82,6 @@
             </ul>
     </div>        
     </center>  
-    <style>
-        body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh; /* Altura completa de la ventana */
-        margin: 0;
-        font-family: Arial, sans-serif;
-        background-color: #f3f3f3; /* Color de fondo suave */
-        }
-        .info{
-            list-style-position: inside;
-            font-size: 25px;
-            
-        }
-
-        h1{
-          font-size: 40px;
-          text-align: justify;
-        }
-
-        .container{
-            max-width: 100%;
-            max-height: 100vh; /* Altura máxima no mayor al 100% del viewport */
-            text-align: justify;
-            background: rgba(211, 64, 64, 0.849);; /* Fondo blanco rojizo */
-            padding: 40px; /* Más padding para un diseño espacioso */
-            border-radius: 12px; /* Bordes más redondeados */
-            box-shadow: 0 12px 30px rgba(0, 0, 0, 0.3); /* Sombra más pronunciada */
-            width: 700px; /* Ajuste del ancho */
-            border: 2px solid #ff9999; /* Borde suave */
-            font-size: 18px;
-        }
-        button:hover {
-            opacity: 1;
-        }
-
-        button:active {
-            top: 4px;
-            box-shadow: #c0392b 0px 3px 2px,#000 0px 3px 5px;
-        }
-        .button2 {
-            display: inline-block;
-            transition: all 0.2s ease-in;
-            position: relative;
-            overflow: hidden;
-            z-index: 1;
-            color: #090909;
-            padding: 0.2em 0,5em;
-            cursor: pointer;
-            font-size: 14px;
-            border-radius: 0.5em;
-            background: #e8e8e8;
-            border: 1px solid #e8e8e8;
-            box-shadow: 1,5px 1,5px 4px #c5c5c5, 0px 0px 3px #ffffff;
-            margin-left: 17px;
-  
-        }
-
-        .button2:active {
-            color: #666;
-            box-shadow: inset 4px 4px 12px #c5c5c5, inset -4px -4px 12px #ffffff;
-        }
-
-        .button2:before {
-            content: "";
-            position: absolute;
-            left: 50%;
-            transform: translateX(-50%) scaleY(1) scaleX(1.25);
-            top: 100%;
-            width: 140%;
-            height: 180%;
-            background-color: rgba(0, 0, 0, 0.05);
-            border-radius: 50%;
-            display: block;
-            transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
-            z-index: -1;
-        }
-
-        .button2:after {
-            content: "";
-            position: absolute;
-            left: 55%;
-            transform: translateX(-50%) scaleY(1) scaleX(1.45);
-            top: 180%;
-            width: 160%;
-            height: 190%;
-            background-color: #fa8560;
-            border-radius: 50%;
-            display: block;
-            transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
-            z-index: -1;
-        }
-
-        .button2:hover {
-            color: #ffffff;
-            border: 1px solid #fa8560;
-        }
-
-        .button2:hover:before {
-            top: -35%;
-            background-color: #fa8560;
-            transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
-        }
-
-        .button2:hover:after {
-            top: -45%;
-            background-color: #fa8560;
-            transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
-        }
-
-    </style>  
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>   
 </body>
 </html>
