f6f0edb83f9883e97a9dcc91b899dc72a5fe6f32
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-10-16 16:18:47 -0300

    mas esilos

--- app/Views/aire.php
+++ app/Views/aire.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo base_url("/css/airstyle.css");?>"> 
-    <link rel="stylesheet" href="<?php echo base_url("/css/estilog.css");?>"> 
+    <link rel="stylesheet" href="<?php echo base_url("/css/defaultstyle.css");?>"> 
     <title>Control Remoto</title>
 </head>
 <body>
--- app/Views/ventilador.php
+++ app/Views/ventilador.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Control Remoto</title>
-    <link rel="stylesheet" href="<?php echo base_url("/css/estilog.css");?>"> 
+    <link rel="stylesheet" href="<?php echo base_url("/css/defaultstyle.css");?>"> 
     <link rel="stylesheet" href="<?php echo base_url("/css/ventilador.css");?>"> 
 </head>
 <body>
