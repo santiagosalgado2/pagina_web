94a1e6264124687b5724172208424449beff6629
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-10-18 14:45:00 -0300

    asdsa

--- app/Views/create_pw.php
+++ app/Views/create_pw.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo base_url("/css/defaultstyle.css");?>"> 
+    <link rel="stylesheet" href="<?php echo base_url("/css/createpwstyle.css");?>"> 
     <title>Document</title>
 </head>
 <body>
