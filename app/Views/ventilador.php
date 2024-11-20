df30315ada0971218fe9e724d554922cae334f2b
catrielgaray27 <catrielgaray@alumnos.itr3.edu.ar>
2024-11-20 16:26:56 -0300

    dsad

--- app/Views/ventilador.php
+++ app/Views/ventilador.php
@@ -86,6 +86,7 @@ $modobrisa_string = implode(',', $modobrisa);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Control Remoto ventilador</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url("/css/ventilador.css");?>"> 
 </head>
 <body>
