f057b0cbfd6cb9eec32fd6b4a1ac11cf82a3ced1
santiagosalgado <santiagosalgado@alumnos.itr3.edu.ar>
2025-02-10 14:24:21 -0300

    t

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,12 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 
 
- $routes->setDefaultNamespace('App\Controllers');
- $routes->setDefaultController('Home');
- $routes->setDefaultMethod('index');
- $routes->setTranslateURIDashes(false);
- $routes->set404Override();
- $routes->setAutoRoute(true);
 
 
 
